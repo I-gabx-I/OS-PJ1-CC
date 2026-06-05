6dc057fc8447299635d5431795a87c49eafa3539
Samuel Caal <157657276+Sabi-0@users.noreply.github.com>
2026-06-05 00:14:25 -0600

    Refactor process table and update scheduling functions

--- os/scheduler.h
+++ os/scheduler.h
@@ -3,17 +3,14 @@
  
 #include "pcb.h"
  
-/* Global process table and current-process index */
-extern pcb_t process_table[3];
+/* Process slots: 0 = OS/idle, 1 = P1, 2 = P2, 3 = P3 */
+#define NPROC 4
+ 
+extern pcb_t process_table[NPROC];
 extern int   current_process;
  
-/* Initialise PCBs and set first task ready                        */
 void init_scheduler(void);
- 
-/* Round-robin: advance current_process skipping TERMINATED tasks  */
-void schedule(void);
- 
-/* Mark a task terminated (called by SYS_EXIT or fault handler)    */
+void schedule(void);   /* round-robin over runnable user tasks 1..NPROC-1 */
 void terminate_process(int pid, int32_t exit_code, uint32_t fault_type);
  
 #endif /* SCHEDULER_H */
